558b4e46d1ec2d2678887fed746ee03a53f4aa18
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-08-29 14:27:37 +0530

    Update settings.py

--- dams/settings.py
+++ dams/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+ STAICFILES_DIRS = [BASE_DIR / 'static']
 
-# STAICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_URL = 'static/'
 
 # Default primary key field type
